e83569bcd8b4b6d108b1edcd00866d7223f8bf1a
SimonIvenPXL <simon.iven@student.pxl.be>
2024-05-27 15:27:50 +0200

    Final changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const data = (fs.readFileSync('./resources/cymbals.json')).toString();
+const data = (fs.readFileSync('./resources/cymbalsLatest.json')).toString();
 const jsonData = JSON.parse(data);
 const cymbalData = [...jsonData.cymbals];
 
@@ -13,18 +13,16 @@ app.use(express.json());
 
 
 app.post('/filter', (req, res) => {
-    const { brightness, dryness, definition, sustain, thickness, bellWidth, bellHeight,
-        hammering, complexity, holes, effect, size} = req.body;
+    const { brightness, dryness, definition, sustain, thickness, bellWidth, complexity, holes, effect, size} = req.body;
 
     const filteredData = applyFilters({brightness, dryness, definition, sustain, thickness,
-        bellWidth, bellHeight, hammering, complexity, holes, effect, size});
+        bellWidth, complexity, holes, effect, size});
     res.json(filteredData);
 });
 
 function applyFilters(filters) {
 
-    const { brightness, dryness, definition, sustain, thickness, bellWidth, bellHeight,
-        hammering, complexity, holes, effect, size} = filters;
+    const { brightness, dryness, definition, sustain, thickness, bellWidth, complexity, holes, effect, size} = filters;
 
     const filteredData = cymbalData.filter(item => {
         const matchesBrightness = (item.Brightness !== undefined && item.Brightness === brightness) || brightness === 0;
@@ -33,16 +31,14 @@ function applyFilters(filters) {
         const matchesSustain = (item.Sustain !== undefined && item.Sustain === sustain) || sustain === 0;
         const matchesThickness = (item.Dikte !== undefined && item.Dikte === thickness) || thickness === 0;
         const matchesBellWidth = (item.Belbreedte !== undefined && item.Belbreedte === bellWidth) || bellWidth === 0;
-        const matchesBellHeight = (item.Belhoogte !== undefined && item.Belhoogte === bellHeight) || bellHeight === 0;
-        const matchesHammering = (item.Hamering !== undefined && item.Hamering === hammering) || hammering === 0;
         const matchesComplexity = (item.Complexiteit !== undefined && item.Complexiteit === complexity) || complexity === 0;
         const matchesHoles = (item.Gaten !== undefined && item.Gaten === holes);
-        const matchesEffect = (item.Effectcymbaal !== undefined && item.Effectcymbaal === holes);
+        const matchesEffect = (item.Effectcymbaal !== undefined && item.Effectcymbaal === effect);
         const matchesSize = (item.hasOwnProperty(size)) || size === "0";
 
         return matchesBrightness && matchesDryness && matchesDefinition &&
-            matchesSustain && matchesThickness && matchesBellWidth && matchesBellHeight &&
-            matchesHammering && matchesComplexity && matchesHoles && matchesEffect && matchesSize;
+            matchesSustain && matchesThickness && matchesBellWidth &&
+            matchesComplexity && matchesHoles && matchesEffect && matchesSize;
     });
 
     return filteredData;
